177b74923703dd4b54200c607adf9542f8c1c75d
Davide <64831836+DavideTW@users.noreply.github.com>
2024-10-10 12:05:53 +0200

    Sistemato bug che non permetteva di inserire le GLOCK negli scambi

--- src/main/java/com/trophonix/tradeplus/trade/Trade.java
+++ src/main/java/com/trophonix/tradeplus/trade/Trade.java
@@ -55,6 +55,7 @@ public class Trade implements Listener {
   public Trade(Player p1, Player p2) {
     player1 = p1;
     player2 = p2;
+    
     location1 = p1.getLocation();
     location2 = p2.getLocation();
     pl.getTaskFactory()
